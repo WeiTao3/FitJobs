f62af0dbbcbc9244830a66c5e7931a2cd9529fb4
Wei <wei.tao@mail.utoronto.ca>
2025-07-29 15:24:08 +0800

    fix deployment blank error

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -3,5 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 // TODO: Replace with your own Supabase project credentials
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY); 
